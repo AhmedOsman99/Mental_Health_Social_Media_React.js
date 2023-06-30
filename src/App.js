cfbda5be602af20a7aeb48c9276b1a31b4a98a10
Muhammad-Galal <muhammadjalal98@gmail.com>
2023-06-30 17:55:00 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 import { Doctor_form } from "./components/Doctor_form";
 import { User_form } from "./components/User_form";
+import { NavBar } from "./components/NavBar";
 import {
   BrowserRouter,
   Navigate,
@@ -13,6 +14,7 @@ import {
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
