d20db7837b115454988985758533099e3508c00d
kareem magdy <kareemmagdy85888@gmail.com>
2023-07-04 20:15:49 +0300

    updated apps

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Profile } from "./components/Profile";
 import { NavBar } from "./components/NavBar";
+import {Doctor_form} from "./components/Doctor_form";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="profile" element={<Profile />} />
+        
+        <Route path="" element={<Doctor_form />} />
+
+
       </Routes>
     </div>
   );
