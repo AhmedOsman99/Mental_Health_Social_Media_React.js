ad712d2ff29339146713049c55d4b901fb1a43ab
[abdelrahman sabry] <[abdelrhmansabry.219@gmail.com]>
2023-07-04 18:59:35 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import "./App.css";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import "./App.css";
+import { PostContextProvider } from "./components/contexts/PostContextProvider";
+import { Doctor_form } from "./components/Doctor_form";
+import { AuthProvider } from "./context/AuthContext";
+import Chat from "./components/chat";
+import { Login } from "./components/Login";
 import { Profile } from "./components/Profile";
 import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="profile" element={<Profile />} />
-      </Routes>
+      <AuthProvider>
+        <PostContextProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="home" element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="" element={<Doctor_form />} />
+          </Routes>
+        </PostContextProvider>
+      </AuthProvider>
     </div>
   );
 }
