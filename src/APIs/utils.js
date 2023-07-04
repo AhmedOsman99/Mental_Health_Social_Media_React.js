105896d922e0c9d2a711f4c6af73f40ff2661dc1
kareem magdy <kareemmagdy85888@gmail.com>
2023-07-04 17:51:41 +0300

    added friendlist,friendrequest and respondtorequest functions

--- src/APIs/utils.js
+++ src/APIs/utils.js
@@ -58,4 +58,40 @@ export const sendRequest = async (recipient_id) => {
     return response
   }
   
+  export const getFriendRequests = async () => {
+    let authTokens = JSON.parse(localStorage.getItem('authTokens'));
+     let accessToken = authTokens.access;
+     console.log(accessToken);
+     let config = {
+       headers: {
+         Authorization: `Bearer ${accessToken}`,
+       },
+     };
+    let response = await axios.get('http://127.0.0.1:8000/connect/list_requests/', config)
+    return response
+}
+
+
+export const respondToFriendRequest = async (friendRequestId, response) => {
+  let authTokens = JSON.parse(localStorage.getItem('authTokens'));
+  let accessToken = authTokens.access;
+  console.log(accessToken);
+  let config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  let data = {
+    friend_request_id: friendRequestId,
+    response: response
+  };
   
+  try {
+    let response = await axios.post('http://127.0.0.1:8000/connect/respond_request/', data, config);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
